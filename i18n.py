22bdd8f8eb4f0cdb2ccee964a90aba86984077ef
ablozhou <ablozhou@cc6d74fa-0a98-11df-beaf-bb271b2086c6>
2010-03-11 05:03:05 +0000

    modify i18n

--- i18n.py
+++ i18n.py
@@ -11,18 +11,22 @@ def install(app, localdir, languages):
         gettext.install('hzdq', localedir = localdir, unicode = True)
     else:
         app.lang = wx.Locale()
-        app.lang.Init(wx.LANGUAGE_DEFAULT)
+        
         if languages[0] == 'zh_CN':
             app.lang.Init(wx.LANGUAGE_CHINESE_SIMPLIFIED)
+        else:
+            app.lang.Init(wx.LANGUAGE_DEFAULT)
         app.lang.AddCatalogLookupPathPrefix(localdir)
         app.lang.AddCatalog('hzdq')
         gettext.install('hzdq', localedir = localdir, unicode = True)
 
 def install1(localdir, languages):
     lang = wx.Locale()
-    lang.Init(wx.LANGUAGE_DEFAULT)
+    
     if languages[0] == 'zh_CN':
         lang.Init(wx.LANGUAGE_CHINESE_SIMPLIFIED)
+    else:
+        lang.Init(wx.LANGUAGE_DEFAULT)
     lang.AddCatalogLookupPathPrefix(localdir)
     lang.AddCatalog('hzdq')
     gettext.install('hzdq', localedir=localdir, unicode=True)
